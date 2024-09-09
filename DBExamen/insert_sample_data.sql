0575b5a1d5f5f6bd4d6747f0328b0ecff14322bd
Victoria <victoria.coronado@externos-cl.cencosud.com>
2024-09-09 18:33:59 -0300

    ⚙ Chore: Scripts to Create Tables and Insert Data to Docker

--- DBExamen/insert_sample_data.sql
+++ DBExamen/insert_sample_data.sql
@@ -1,15 +1,13 @@
--- Script to insert sample data into the tables
-
--- Insert genders
+-- Insertar géneros
 INSERT INTO GENDERS (ID, NAME) VALUES (1, 'Male');
-INSERT INTO GENDERS (ID, NAME) VALUES (2, 'Male');
+INSERT INTO GENDERS (ID, NAME) VALUES (2, 'Female');
 
--- Insert jobs
+-- Insertar trabajos
 INSERT INTO JOBS (ID, NAME, SALARY) VALUES (1, 'Backend Engineer', 5000);
 INSERT INTO JOBS (ID, NAME, SALARY) VALUES (2, 'Frontend Engineer', 5000);
 
--- Insert employees
-INSERT INTO EMPLOYEES (ID, GENDER_ID, JOB_ID, NAME, LAST_NAME, BIRTHDATE) 
-VALUES (1, 1, 1, 'Juan', 'Pérez', TO_DATE('1995-01-06', 'YYYY-MM-DD'));
-INSERT INTO EMPLOYEES (ID, GENDER_ID, JOB_ID, NAME, LAST_NAME, BIRTHDATE) 
-VALUES (2, 2, 2, 'Mariana', 'García', TO_DATE('1985-02-18', 'YYYY-MM-DD'));
+-- Insertar empleados
+INSERT INTO EMPLOYEES (ID, GENDER_ID, JOB_ID, NAME, LAST_NAME, BIRTHDATE)
+VALUES (1, 1, 1, 'Juan', 'Pérez', '1995-01-06');
+INSERT INTO EMPLOYEES (ID, GENDER_ID, JOB_ID, NAME, LAST_NAME, BIRTHDATE)
+VALUES (2, 2, 2, 'Mariana', 'García', '1985-02-18');
