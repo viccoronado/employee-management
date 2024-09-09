991e018896ca846d0b738abedd96aa470f74b2f5
Victoria <victoria.coronado@externos-cl.cencosud.com>
2024-09-09 18:31:24 -0300

    ⚙ Chore: Add Docker Image

--- DBExamen/create_tables.sql
+++ DBExamen/create_tables.sql
@@ -1,30 +1,35 @@
+-- create_tables.sql
+
 -- GENDERS
 CREATE TABLE GENDERS (
-    ID NUMBER PRIMARY KEY,
-    NAME VARCHAR2(255) NOT NULL
+                         ID INT PRIMARY KEY,
+                         NAME VARCHAR(255) NOT NULL
 );
 
 -- JOBS
 CREATE TABLE JOBS (
-    ID NUMBER PRIMARY KEY,
-    NAME VARCHAR2(255) NOT NULL,
-    SALARY NUMBER(9, 2)
+                      ID INT PRIMARY KEY,
+                      NAME VARCHAR(255) NOT NULL,
+                      SALARY DECIMAL(9, 2)
 );
 
 -- EMPLOYEES
 CREATE TABLE EMPLOYEES (
-    ID NUMBER PRIMARY KEY,
-    GENDER_ID NUMBER REFERENCES GENDERS(ID),
-    JOB_ID NUMBER REFERENCES JOBS(ID),
-    NAME VARCHAR2(255) NOT NULL,
-    LAST_NAME VARCHAR2(255) NOT NULL,
-    BIRTHDATE DATE
+                           ID INT PRIMARY KEY,
+                           GENDER_ID INT,
+                           JOB_ID INT,
+                           NAME VARCHAR(255) NOT NULL,
+                           LAST_NAME VARCHAR(255) NOT NULL,
+                           BIRTHDATE DATE,
+                           FOREIGN KEY (GENDER_ID) REFERENCES GENDERS(ID),
+                           FOREIGN KEY (JOB_ID) REFERENCES JOBS(ID)
 );
 
 -- EMPLOYEE_WORKED_HOURS
 CREATE TABLE EMPLOYEE_WORKED_HOURS (
-    ID NUMBER PRIMARY KEY,
-    EMPLOYEE_ID NUMBER REFERENCES EMPLOYEES(ID),
-    WORKED_HOURS NUMBER(9, 2),
-    WORKED_DATE DATE
+                                       ID INT PRIMARY KEY,
+                                       EMPLOYEE_ID INT,
+                                       WORKED_HOURS DECIMAL(9, 2),
+                                       WORKED_DATE DATE,
+                                       FOREIGN KEY (EMPLOYEE_ID) REFERENCES EMPLOYEES(ID)
 );
